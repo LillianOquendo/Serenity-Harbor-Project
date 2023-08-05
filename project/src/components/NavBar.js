0d72d4d113e4f66f59a2b26168c8488106cf5c13
Lillian Oquendo <LillianOquendo1@gmail.com>
2023-08-05 19:34:38 -0400

    navbar almost done, still needs logo

--- project/src/components/NavBar.js
+++ project/src/components/NavBar.js
@@ -2,29 +2,76 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function NavBar() {
+    // Header
+    const headerStyle = {
+        color: '#F0F3BD',
+        fontFamily: 'Arial, sans-serif',
+        
+    };
+
+    const containerStyle = {
+        //Container
+        margin: 'auto',
+        padding: '5px',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#05668D'
+        
+    };
+
+    const logoStyle = {
+        //Logo
+        width: '10px',
+        height: '10px',
+        color: 'white',
+        padding: '20px',
+        backgroundColor: '#FFD700',
+        borderRadius: '50%',
+        
+    };
+
+    const linkStyle = {
+        //Links
+        marginRight: '30px',
+        color: '#F0F3BD',
+        textDecoration: 'none',
+        
+    };
+
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/safety_plan">Safety Plan</Link>
-                </li>
-                <li>
-                    <Link to="/contact">Contact</Link>
-                </li>
-                <li>
-                    <Link to ="/testimonials">Testimonials</Link>
-                </li>
-                <li>
-                    <Link to ="/id_abuse"> Signs of Abuse</Link>
-                </li>
-                <li>
-                    <Link to ="/consultations"> Consultations</Link>
-                </li>
-            </ul>
-        </nav>
+        <header style={headerStyle}>
+            <div style={containerStyle}>
+                <Link to="/" style={{ ...logoStyle, ...{ display: 'flex', alignItems: 'center' } }}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" style={{ flex: 'none' }} viewBox="0 0 24 24" data-darkreader-inline-stroke="">
+                        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+                    </svg>
+                    <span style={{ marginLeft: '30px' }} className="text-xl">
+                        Serenity Harbor
+                    </span>
+                </Link>
+                <nav style={{ marginLeft: 'auto' }}>
+                    <Link to="/" style={linkStyle}>
+                        Home
+                    </Link>
+                    <Link to="/safety_plan" style={linkStyle}>
+                        Safety Plan
+                    </Link>
+                    <Link to="/id_abuse" style={linkStyle}>
+                        Signs of Abuse
+                    </Link>
+                    <Link to="/testimonials" style={linkStyle}>
+                        Testimonials
+                    </Link>
+                    <Link to="/consultations" style={linkStyle}>
+                        Consultations
+                    </Link>
+                    <Link to="/contact" style={linkStyle}>
+                        Contact
+                    </Link>
+                </nav>
+            </div>
+        </header>
     );
 }
 
