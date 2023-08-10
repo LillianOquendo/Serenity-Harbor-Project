c27ab7caf4535348233d7a0c730fe81a89800162
Lillian Oquendo <LillianOquendo1@gmail.com>
2023-08-10 05:12:09 -0400

    id abuse css

--- project/src/components/IdAbuse.js
+++ project/src/components/IdAbuse.js
@@ -5,49 +5,145 @@ import React from 'react';
 
 function IdAbuse() {
     // Example data for types of abuse
-    const abuseData = [
-        {
-            id: 1,
-            type: 'Physical Abuse',
-            description: `Physical abuse is a readily recognizable form of mistreatment that revolves around the utilization
-        of physical violence or the threat of it to exert control
-        over an individual. As a consequence, survivors live in fear
-        and uncertainty, anticipating further instances of abuse.
-        This often reinforces the regular application of other, 
-        more covert forms of abuse.`,
-        },
-        {
-            id: 2,
-            type: 'Emotional Abuse',
-            description: `Emotional abuse, also known as psychological abuse, is a form of mistreatment where one person
-        seeks to control, manipulate, or undermine the emotional well-being and self-esteem of another individual.
-        This type of abuse does not involve physical violence but relies on various tactics aimed at causing emotional
-        pain, fear, and confusion. Emotional abuse can occur in personal relationships, such as within families, 
-        romantic partnerships, friendships, or workplace dynamics.`,
-        },
-        {
-            id: 3,
-            type: 'Verbal Abuse',
-            description: `Financial abuse is a form of control and manipulation that occurs when one person exerts power
-        over another individual's financial resources or economic well-being to gain dominance and limit their independence.
-        This type of abuse is often associated with intimate partner relationships, where the abuser exploits the victim's
-        financial vulnerabilities and resources to maintain control. However, it can also happen in other contexts, such
-        as family settings or caregiver relationships.`,
-        },
-
-    ];
 
     return (
-        <div>
-            <h2>Types of Abuse</h2>
-            {abuseData.map((abuse) => (
-                <div key={abuse.id}>
-                    <h3>{abuse.type}</h3>
-                    <p>{abuse.description}</p>
-                </div>
-            ))}
+        <div className="bg-gradient-to-r from-cyan-500 to-blue-500 p-20">
+  <div class="mx-auto max-w-screen-xl px-4 md:px-8">
+    {/* <!-- text - start --> */}
+    <div class="mb-10 md:mb-16">
+      <h2 class="mb-4 text-center text-2xl font-bold text-white md:mb-6 lg:text-6xl">Types of Abuse</h2>
+          <p class="mx-auto max-w-screen-md text-center text-white md:text-2xl">Abuse comes in many forms. Below you'll find indicators of various abuse forms. Please use it as a guide. </p>
+         
+    </div>
+    {/* <!-- text - end --> */}
+
+    <div class="grid gap-4 sm:grid-cols-2 md:gap-6 lg:grid-cols-2 xl:grid-cols-2 xl:gap-8">
+      {/* <!-- article - start --> */}
+      <div class="flex flex-col items-center overflow-hidden rounded-lg border md:flex-row">
+        <a href="#" class="group relative block h-48 w-full shrink-0 self-start overflow-hidden bg-gray-100 md:h-full md:w-32 lg:w-48">
+          <img src="https://i.ibb.co/dkX5mQb/abuse-image7.jpg" loading="lazy" alt="Photo by Minh Pham" class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
+        </a>
+
+        <div class="flex flex-col gap-2 p-4 lg:p-6">
+          <h2 class="text-xl font-bold text-white">
+            <a href="#" class="transition duration-100 hover:text-indigo-500 active:text-indigo-600">Financial Abuse</a>
+          </h2>
+
+          <p class="text-white">`Financial abuse is a form of control and manipulation that occurs when one person exerts power
+        over another individual's financial resources or economic well-being to gain dominance and limit their independence.</p>
+  
+        
         </div>
-    );
+      </div>
+      {/* <!-- article - end -->
+
+      <!-- article - start --> */}
+      <div class="flex flex-col items-center overflow-hidden rounded-lg border md:flex-row">
+        <a href="#" class="group relative block h-48 w-full shrink-0 self-start overflow-hidden bg-gray-100 md:h-full md:w-32 lg:w-48">
+          <img src="https://i.ibb.co/dP6Mvbv/abuse-image1.jpg" loading="lazy" alt="Photo by Lorenzo Herrera" class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
+        </a>
+
+        <div class="flex flex-col gap-2 p-4 lg:p-6">
+         
+
+          <h2 class="text-xl font-bold text-white">
+            <a href="#" class="transition duration-100 hover:text-indigo-500 active:text-indigo-600">Emotional & Verbal Abuse</a>
+          </h2>
+
+          <p class="text-white">Emotional abuse includes non-physical behaviors such as threats, insults, constant monitoring or 'checking-in,' 
+          excessive texting, humiliation, intimidation, isolation, or stalking. Relationships can still be unhealthy or abusive even without physical abuse. </p>
+
+         
+        </div>
+      </div>
+      {/* <!-- article - end -->
+
+      <!-- article - start --> */}
+      <div class="flex flex-col items-center overflow-hidden rounded-lg border md:flex-row">
+        <a href="#" class="group relative block h-48 w-full shrink-0 self-start overflow-hidden bg-gray-100 md:h-full md:w-32 lg:w-48">
+          <img src="https://i.ibb.co/ssTD5FD/abuse-image2.jpg" loading="lazy" alt="Photo by Magicle" class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
+        </a>
+
+        <div class="flex flex-col gap-2 p-4 lg:p-6">
+       
+
+          <h2 class="text-xl font-bold text-white">
+            <a href="#" class="transition duration-100 hover:text-indigo-500 active:text-indigo-600">Physical Abuse</a>
+          </h2>
+
+          <p class="text-white">Physical abuse is any intentional, unwanted contact with you or something close to your body,
+           or any behavior that causes or has the intention of causing you injury, disability or death. Abusive behavior may not always 
+           cause physical pain or leave a bruise, bru it's still unhealthy and should always be taken seriously.</p>
+
+         
+        </div>
+      </div>
+      {/* <!-- article - end --> */}
+
+      {/* <!-- article - start --> */}
+      <div class="flex flex-col items-center overflow-hidden rounded-lg border md:flex-row">
+        <a href="#" class="group relative block h-48 w-full shrink-0 self-start overflow-hidden bg-gray-100 md:h-full md:w-32 lg:w-48">
+          <img src="https://i.ibb.co/K2xW35Y/abuse-image6.jpg" loading="lazy" alt="Photo by Martin Sanchez" class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
+        </a>
+
+        <div class="flex flex-col gap-2 p-4 lg:p-6">
+     
+
+          <h2 class="text-xl font-bold text-white">
+            <a href="#" class="transition duration-100 hover:text-indigo-500 active:text-indigo-600">Sexual Abuse</a>
+          </h2>
+
+          <p class="text-white">Sexual Abuse refers to any behavior that pressures or coerces someone to do something sexually that they do not what to do. 
+          It can also refer to behavior that impacts a person's ability to control their sexual activity or the circumstances in which sexual activity takes place, 
+          including oral sex, rape, or controlling reproductive methods and choices</p>
+
+         
+        </div>
+      </div>
+      {/* <!-- article - end --> */}
+            {/* <!-- article - start --> */}
+            <div class="flex flex-col items-center overflow-hidden rounded-lg border md:flex-row">
+        <a href="#" class="group relative block h-48 w-full shrink-0 self-start overflow-hidden bg-gray-100 md:h-full md:w-32 lg:w-48">
+          <img src="https://i.ibb.co/HxLtzFb/abuse-image4.jpg" loading="lazy" alt="Photo by Martin Sanchez" class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
+        </a>
+
+        <div class="flex flex-col gap-2 p-4 lg:p-6">
+         
+
+          <h2 class="text-xl font-bold text-white">
+            <a href="#" class="transition duration-100 hover:text-indigo-500 active:text-indigo-600">Digital Abuse</a>
+          </h2>
+
+          <p class="text-white">Digital dating abuse is the use of technologies like texting and social media to bully, harass, stalk, or intimidate a partner.
+          This behavior is often a form of verbal or emotional abuse, conducted online.</p>
+
+          
+        </div>
+      </div>
+      {/* <!-- article - end --> */}
+            {/* <!-- article - start --> */}
+            <div class="flex flex-col items-center overflow-hidden rounded-lg border md:flex-row">
+        <a href="#" class="group relative block h-48 w-full shrink-0 self-start overflow-hidden bg-gray-100 md:h-full md:w-32 lg:w-48">
+          <img src="https://i.ibb.co/87L0TcB/abuse-image5.jpg" loading="lazy" alt="Photo by Martin Sanchez" class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
+        </a>
+
+        <div class="flex flex-col gap-2 p-4 lg:p-6">
+        
+
+          <h2 class="text-xl font-bold text-white">
+            <a href="#" class="transition duration-100 hover:text-indigo-500 active:text-indigo-600">Stalking</a>
+          </h2>
+
+          <p class="text-white">Stalking occurs when someone watches, follows, or harasses you repeatedly, making you feel afraid or unsafe. A stalker 
+          can be someone you know, a past partner, or a stranger. When reviewing this list please keep in mind that the definition of stalking varies from state to state.</p>
+
+          
+        </div>
+      </div>
+      {/* <!-- article - end --> */}
+    </div>
+  </div>
+</div>    );
 }
 
 export default IdAbuse;
