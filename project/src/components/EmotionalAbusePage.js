0213d2e94ab2408d777af4ef596b6a87e96a4c54
Lillian <LillianOquendo1@gmail.com>
2023-08-21 23:52:01 -0400

    emotional abuse page formatted and filled

--- project/src/components/EmotionalAbusePage.js
+++ project/src/components/EmotionalAbusePage.js
@@ -3,7 +3,77 @@ import React from "react";
 function Emotional(){
     return(
         <div>
-            <h1>Emotional</h1>
+                    <div>
+                <div>
+
+
+<div>
+<div class = "md:lg:xl:grid-cols-3 group bg-white shadow-xl shadow-neutral-100 border">
+    <div
+    class="p-10 flex flex-col items-center text-center group md:lg:xl:border-r md:lg:xl:border-b hover:bg-slate-50 cursor-default">
+        
+        <p class="text-5xl font-medium text-slate-700 mt-3">Emotional/Verbal Abuse</p>
+                <p class="p-5 text-center mt-2 text-lg text-slate-500">Emotional abuse includes non-physical behaviors such as threats, insults, constant monitoring or 'checking-in,'
+                                excessive texting, humiliation, intimidation, isolation, or stalking. Relationships can still be unhealthy or abusive even without physical abuse. </p>
+                    <h2 class="text-2xl font-medium text-slate-700 mt-3">Signs of Emotional and/or Verbal Abuse</h2>
+    </div>
+
+</div>
+        <div class="grid grid-cols-1 md:lg:xl:grid-cols-3 group bg-white shadow-xl shadow-neutral-100 border ">
+            
+
+            <div
+                class="p-10 flex flex-col items-center text-center group md:lg:xl:border-r md:lg:xl:border-b hover:bg-slate-50 cursor-default">
+                <span class="p-5 rounded-full bg-red-500 text-white shadow-lg shadow-red-200"></span>
+                <p class="text-xl font-medium text-slate-700 mt-3">Constantly Insulting You </p>
+                <p class="mt-2 text-sm text-slate-500">Calling you names, insulting you, or constantly criticizing you: Verbal abuse, including name-calling and criticism, can have a profound impact on a person's self-esteem and mental health</p>
+            </div>
+
+            <div
+                class="p-10 flex flex-col items-center text-center group md:lg:xl:border-r md:lg:xl:border-b hover:bg-slate-50 cursor-default">
+                <span class="p-5 rounded-full bg-orange-500 text-white shadow-lg shadow-orange-200"></span>
+                <p class="text-xl font-medium text-slate-700 mt-3">Isolation</p>
+                <p class="mt-2 text-sm text-slate-500">Isolating you from family, friends, or other people in your life because it makes someone easier to control: Isolation is a common tactic used by abusers to control their victims and limit their support networks.</p>
+            </div>
+
+            <div class="p-10 flex flex-col items-center text-center group   md:lg:xl:border-b hover:bg-slate-50 cursor-default">
+                <span class="p-5 rounded-full bg-yellow-500 text-white shadow-lg shadow-yellow-200"></span>
+                <p class="text-xl font-medium text-slate-700 mt-3">Excessive Monitoring</p>
+                <p class="mt-2 text-sm text-slate-500">Monitoring your activities with or without your knowledge: Excessive monitoring and surveillance can be a form of control and invasion of privacy, leading to emotional distress.</p>
+            </div>
+
+
+            <div class="p-10 flex flex-col items-center text-center group   md:lg:xl:border-r hover:bg-slate-50 cursor-default">
+                <span class="p-5 rounded-full bg-lime-500 text-white shadow-lg shadow-lime-200"></span>
+                <p class="text-xl font-medium text-slate-700 mt-3">Gaslighting</p>
+                <p class="mt-2 text-sm text-slate-500">Gaslighting involves manipulating your perception of reality and can lead to confusion, self-doubt, and emotional turmoil.</p>
+            </div>
+
+            <div class="p-10 flex flex-col items-center text-center group    md:lg:xl:border-r hover:bg-slate-50 cursor-default">
+                <span class="p-5 rounded-full bg-teal-500 text-white shadow-lg shadow-teal-200"></span>
+                <p class="text-xl font-medium text-slate-700 mt-3">Blame</p>
+                <p class="mt-2 text-sm text-slate-500">Blaming you for their abusive behaviors: This involves the abuser shifting responsibility for their actions onto the victim, making the victim feel guilty or responsible for the abuse they endure. It is a form of emotional manipulation and control.</p>
+            </div>
+
+            <div class="p-10 flex flex-col items-center text-center group     hover:bg-slate-50 cursor-default">
+                <span class="p-5 rounded-full bg-indigo-500 text-white shadow-lg shadow-indigo-200"></span>
+                <p class="text-xl font-medium text-slate-700 mt-3">Conditional Love Claims</p>
+                <p class="mt-2 text-sm text-slate-500">Telling you that you're lucky to be with them and that you'll never find someone better: This is a form of manipulation aimed at undermining your self-worth and self-esteem, making you feel trapped in the relationship.</p>
+            </div>
+
+
+
+
+        </div>
+
+        {/* <div class="w-full   bg-indigo-600 shadow-xl shadow-indigo-200 py-10 px-20 flex justify-between items-center">
+            <p class=" text-white"> <span class="text-4xl font-medium">Still Confused ?</span> <br></br> <span class="text-lg">Book For Free Career Consultation Today ! </span></p>
+            <button class="px-5 py-3  font-medium text-slate-700 shadow-xl  hover:bg-white duration-150  bg-yellow-400">BOOK AN APPOINTMENT </button>
+        </div> */}
+
+    </div>
+        </div>
+        </div>
         </div>
     )
 }
