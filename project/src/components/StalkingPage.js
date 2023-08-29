f65ab9783a1bfb0bb7afb409008b779836e905a8
Lillian <LillianOquendo1@gmail.com>
2023-08-29 01:46:27 -0400

    abuse pages css updated

--- project/src/components/StalkingPage.js
+++ project/src/components/StalkingPage.js
@@ -1,74 +1,136 @@
 import React from "react";
-
+import NewsletterSignup from "./Newsletter";
 function Stalking(){
     return(
+        
+        <div>
+        <div className="flex items-center">
         <div>
 
+        </div>
+        <div className="w-1/2  m-20">
+            <div>
+            <div>
+                <div className="mt-5 bg-white rounded-tr-3xl rounded-tl-3xl rounded-br-3xl rounded-bl-3xl">
+                <div className="p-5 flex flex-col items-center text-center group cursor-default">
+                    <p className="text-5xl font-medium text-slate-700 mt-3 ">
+                    Stalking
+                    </p>
+                    <p className="text-center mt-2 text-lg text-slate-500">
+                    Stalking occurs when someone watches, follows, or harasses you repeatedly, making you feel afraid or unsafe. A stalker
+                    can be someone you know, a past partner, or a stranger.
+                    <br></br>
+                    When reviewing this list please keep in mind that the definition of stalking varies from state to state.
+                    </p>
+                    <br></br>
+                    <h2 className="text-2xl font-medium text-slate-700 mt-3">
+                    5 Signs of Stalking
+                    </h2>
+                    <br></br>
+                </div>
+                <div className="title-grid text-center"></div>
+                </div>
+                <div>
+                <div className="grid grid-cols-5 group bg-white rounded-tl-3xl rounded-tr-3xl rounded-br-3xl rounded-bl-3xl">
+                    <div className="p-5 flex flex-col items-center text-center group md:lg:xl:border-r cursor-default">
+                    <span className="p-5 rounded-full bg-red-500 text-white shadow-lg shadow-gray-500"></span>
+                    <p className="text-xl font-medium text-slate-700 mt-3">
+                    Unwanted Communication or Items
+                    </p>
+                    <p className="mt-2 text-sm text-slate-500">
+                    Sending you unwanted texts, messages, letters, emails, voicemails, or leaving you unwanted items, gifts, or flowers: These behaviors involve persistent and unwanted contact, which can be distressing.
+                    </p>
+                    </div>
 
-        <div>
-        <div class = "md:lg:xl:grid-cols-3 group bg-white shadow-xl shadow-neutral-100 border">
-            <div
-            class="p-10 flex flex-col items-center text-center group md:lg:xl:border-r md:lg:xl:border-b hover:bg-slate-50 cursor-default">
-                
-                <p class="text-5xl font-medium text-slate-700 mt-3">Stalking</p>
-                        <p class="p-5 text-center mt-2 text-lg text-slate-500">Stalking occurs when someone watches, follows, or harasses you repeatedly, making you feel afraid or unsafe. A stalker
-                                can be someone you know, a past partner, or a stranger.
-                                <br></br> When reviewing this list please keep in mind that the definition of stalking varies from state to state.</p>
-                            <h2 class="text-2xl font-medium text-slate-700 mt-3">Signs of Stalking</h2>
+                    <div className="p-5 flex flex-col items-center text-center group md:lg:xl:border-r cursor-default">
+                    <span className="p-5 rounded-full bg-red-500 text-white shadow-lg shadow-gray-500"></span>
+                    <p className="text-xl font-medium text-slate-700 mt-3">
+                    Uninvited Presence
+                    </p>
+                    <p className="mt-2 text-sm text-slate-500">
+                    Waiting around at places you spend time: This involves stalking behavior where the person shows up unannounced and uninvited at locations you frequent, creating a sense of intrusion and fear.
+                    </p>
+                </div>
+
+                    <div className="p-5 flex flex-col items-center text-center group md:lg:xl:border-r cursor-default">
+                    <span className="p-5 rounded-full bg-red-500 text-white shadow-lg shadow-gray-500"></span>
+                    <p className="text-xl font-medium text-slate-700 mt-3">
+                    Unwanted Surveillance
+                    </p>
+                    <p className="mt-2 text-sm text-slate-500">
+                    Hiring a private investigator to follow or find you as a way of knowing your location or movements: Employing a private investigator to track your movements is a clear sign of stalking and invasion of privacy.
+                    </p>
+                    </div>
+
+                    <div className="p-5 flex flex-col items-center text-center group md:lg:xl:border-r cursor-default">
+                    <span className="p-5 rounded-full bg-red-500 text-white shadow-lg shadow-gray-500"></span>
+                    <p className="text-xl font-medium text-slate-700 mt-3">
+                    Social Media Tracking
+                    </p>
+                    <p className="mt-2 text-sm text-slate-500">
+                    Using social media or technology to track your activities: Digital stalking and tracking can create a constant sense of surveillance and invasion of privacy.
+                    </p>
+                    </div>
+
+                    <div className="p-5 flex flex-col items-center text-center group cursor-default">
+                    <span className="p-5 rounded-full bg-red-500 text-white shadow-lg shadow-gray-500"></span>
+                    <p className="text-xl font-medium text-slate-700 mt-3">
+                    Defamation
+                    </p>
+                    <p className="mt-2 text-sm text-slate-500">
+                    Spreading rumors about you online or in person: Public defamation can harm your reputation and emotional well-being.
+                    </p>
+                    </div>
+                    
+                </div>
+                </div>
             </div>
+            </div>
+        </div>
+
+        <div className="p-20 w-1/2">
+            <img
+            className="rounded-3xl"
+            
+            src="/assets/images/stalker.jpg"
+            alt="Image by PublicDomainPictures on Pixabay"
+            />
+        </div>
+        
+
+        </div>
+        <div>
+        <div class="relative -mt-12 lg:-mt-24">
+        <svg       
+        viewBox="0 0 1428 174"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink">
+            <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+            <g transform="translate(-2.000000, 44.000000)" fill="#FFFFFF" fill-rule="nonzero">
+                <path d="M0,0 C90.7283404,0.927527913 147.912752,27.187927 291.910178,59.9119003 C387.908462,81.7278826 543.605069,89.334785 759,82.7326078 C469.336065,156.254352 216.336065,153.6679 0,74.9732496" opacity="0.100000001"></path>
+                <path
+                d="M100,104.708498 C277.413333,72.2345949 426.147877,52.5246657 546.203633,45.5787101 C666.259389,38.6327546 810.524845,41.7979068 979,55.0741668 C931.069965,56.122511 810.303266,74.8455141 616.699903,111.243176 C423.096539,147.640838 250.863238,145.462612 100,104.708498 Z"
+                opacity="0.100000001"
+                ></path>
+                <path d="M1046,51.6521276 C1130.83045,29.328812 1279.08318,17.607883 1439,40.1656806 L1439,120 C1271.17211,77.9435312 1140.17211,55.1609071 1046,51.6521276 Z" id="Path-4" opacity="0.200000003"></path>
+            </g>
+            <g transform="translate(-4.000000, 76.000000)" fill="#FFFFFF" fill-rule="nonzero">
+                <path
+                d="M0.457,34.035 C57.086,53.198 98.208,65.809 123.822,71.865 C181.454,85.495 234.295,90.29 272.033,93.459 C311.355,96.759 396.635,95.801 461.025,91.663 C486.76,90.01 518.727,86.372 556.926,80.752 C595.747,74.596 622.372,70.008 636.799,66.991 C663.913,61.324 712.501,49.503 727.605,46.128 C780.47,34.317 818.839,22.532 856.324,15.904 C922.689,4.169 955.676,2.522 1011.185,0.432 C1060.705,1.477 1097.39,3.129 1121.236,5.387 C1161.703,9.219 1208.621,17.821 1235.4,22.304 C1285.855,30.748 1354.351,47.432 1440.886,72.354 L1441.191,104.352 L1.121,104.031 L0.457,34.035 Z"
+                ></path>
+            </g>
+            </g>
+        </svg> 
         
         </div>
-                <div class="grid grid-cols-1 md:lg:xl:grid-cols-3 group bg-white shadow-xl shadow-neutral-100 border ">
-                    
-        
-                    <div
-                        class="p-10 flex flex-col items-center text-center group md:lg:xl:border-r md:lg:xl:border-b hover:bg-slate-50 cursor-default">
-                        <span class="p-5 rounded-full bg-red-500 text-white shadow-lg shadow-red-200"></span>
-                        <p class="text-xl font-medium text-slate-700 mt-3">Unwanted Communication or Items</p>
-                        <p class="mt-2 text-sm text-slate-500">Sending you unwanted texts, messages, letters, emails, voicemails, or leaving you unwanted items, gifts, or flowers: These behaviors involve persistent and unwanted contact, which can be distressing.</p>
-                    </div>
-        
-                    <div
-                        class="p-10 flex flex-col items-center text-center group md:lg:xl:border-r md:lg:xl:border-b hover:bg-slate-50 cursor-default">
-                        <span class="p-5 rounded-full bg-orange-500 text-white shadow-lg shadow-orange-200"></span>
-                        <p class="text-xl font-medium text-slate-700 mt-3">Uninvited Presence</p>
-                        <p class="mt-2 text-sm text-slate-500">Waiting around at places you spend time: This involves stalking behavior where the person shows up unannounced and uninvited at locations you frequent, creating a sense of intrusion and fear.</p>
-                    </div>
-        
-                    <div class="p-10 flex flex-col items-center text-center group   md:lg:xl:border-b hover:bg-slate-50 cursor-default">
-                        <span class="p-5 rounded-full bg-yellow-500 text-white shadow-lg shadow-yellow-200"></span>
-                        <p class="text-xl font-medium text-slate-700 mt-3">Unwanted Surveillance</p>
-                        <p class="mt-2 text-sm text-slate-500">Hiring a private investigator to follow or find you as a way of knowing your location or movements: Employing a private investigator to track your movements is a clear sign of stalking and invasion of privacy.</p>
-                    </div>
-        
-        
-                    <div class="p-10 flex flex-col items-center text-center group   md:lg:xl:border-r hover:bg-slate-50 cursor-default">
-                        <span class="p-5 rounded-full bg-lime-500 text-white shadow-lg shadow-lime-200"></span>
-                        <p class="text-xl font-medium text-slate-700 mt-3">Social Media Tracking</p>
-                        <p class="mt-2 text-sm text-slate-500">Using social media or technology to track your activities: Digital stalking and tracking can create a constant sense of surveillance and invasion of privacy.</p>
-                    </div>
-        
-                    <div class="p-10 flex flex-col items-center text-center group    md:lg:xl:border-r hover:bg-slate-50 cursor-default">
-                        <span class="p-5 rounded-full bg-teal-500 text-white shadow-lg shadow-teal-200"></span>
-                        <p class="text-xl font-medium text-slate-700 mt-3">Defamation</p>
-                        <p class="mt-2 text-sm text-slate-500">Spreading rumors about you online or in person: Public defamation can harm your reputation and emotional well-being.</p>
-                    </div>
-        
-                    <div class="p-10 flex flex-col items-center text-center group     hover:bg-slate-50 cursor-default">
-                        <span class="p-5 rounded-full bg-indigo-500 text-white shadow-lg shadow-indigo-200"></span>
-                        <p class="text-xl font-medium text-slate-700 mt-3">Damaging Property</p>
-                        <p class="mt-2 text-sm text-slate-500">Damaging your home, car, or other property: Property damage is a clear act of aggression that can escalate the sense of danger and fear.</p>
-                    </div>
-        
-        
-        
-        
-                </div>
-        
-
-        
-            </div>
-                </div>
-    )
+            
+        <div class="white-rectangle"></div>
+    
+        <NewsletterSignup />  
+        </div>
+        </div>
+    );
 }
+
 export default Stalking;
